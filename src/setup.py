1897e1c07a937fef9494c4ca2df17c5b1701e2fb
sjwarren <devel@rachandstu.com>
2014-11-11 23:23:21 +1100

    Added distutils support.

---
+++ src/setup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='mippy',
+      version='0.1',
+      description='Wowwee MIP library',
+      author='Stuart Warren',
+      author_email='devel@rachandstu.com',
+      url='https://github.com/vlimit/mip',
+      packages=['mippy']
+     )
+
